bc237e4521790ad09d0cad469f9fb47d8cc4bdc5
KongWill <1272688590@qq.com>
2018-03-25 12:37:42 +0800

    写入44444

--- src/main/java/com/will/DuGuJiuJian.java
+++ src/main/java/com/will/DuGuJiuJian.java
@@ -7,6 +7,7 @@ public class DuGuJiuJian {
 		System.out.println("111111111111");
 		System.out.println("222222222222");
 		System.out.println("3333333333333");
+		System.out.println("4444444444444");
 		
 	}
 
